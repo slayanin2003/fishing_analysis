0ec652684a03c2569b21ca7a1b73d81d751f93f1
Мосунов <mosunov2003@mail.ru>
2024-12-21 20:33:03 +0500

    Откорректирован лог файл

---
+++ fishing_analysis/js_scripts.py
@@ -0,0 +1,10 @@
+js_func = """
+function refresh() {
+    const url = new URL(window.location);
+
+    if (url.searchParams.get('__theme') !== 'dark') {
+        url.searchParams.set('__theme', 'dark');
+        window.location.href = url.href;
+    }
+}
+"""
